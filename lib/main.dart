3b14dac03a06ed27ccee75c3f763eaee6b360eff
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-04-14 23:58:28 +0800

    fdssf

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,240 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'contact.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('database');
+  runApp(CupertinoApp(
+    theme: CupertinoThemeData(brightness: Brightness.dark),
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var box = Hive.box('database');
+  List<Map<String, dynamic>> contacts = [];
+  List<Map<String, dynamic>> filteredContacts = [];
+
+  TextEditingController _searchController = TextEditingController();
+  TextEditingController _fnameController = TextEditingController();
+  TextEditingController _lnameController = TextEditingController();
+  List<TextEditingController> _phoneControllers = [];
+  List<String?> _phoneLabels = [];
+  List<TextEditingController> _emailControllers = [];
+  List<String?> _emailLabels = [];
+  List<TextEditingController> _urlControllers = [];
+  List<String?> _urlLabels = [];
+  String? _imagePath;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadContacts();
+
+    // Load saved image path from local storage
+    _imagePath = box.get('imagePath', defaultValue: null);
+
+    _searchController.addListener(_filterContacts);
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  void _loadContacts() {
+    var storedContacts = box.get('contacts');
+    if (storedContacts is List) {
+      contacts = storedContacts.map((item) {
+        if (item is Map) {
+          return item.map<String, dynamic>((key, value) {
+            return MapEntry(key.toString(), value);
+          });
+        }
+        return <String, dynamic>{};
+      }).toList();
+      contacts.sort((a, b) => (a['name'] as String).toLowerCase().compareTo((b['name'] as String).toLowerCase()));
+      filteredContacts = List.from(contacts);
+    } else {
+      contacts = [];
+      filteredContacts = [];
+    }
+  }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _filterContacts() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      filteredContacts = contacts
+          .where((contact) =>
+          (contact['name'] as String)
+              .toLowerCase()
+              .contains(_searchController.text.toLowerCase()))
+          .toList();
+      filteredContacts.sort((a, b) => (a['name'] as String).toLowerCase().compareTo((b['name'] as String).toLowerCase()));
     });
   }
 
+  void _saveContacts() {
+    box.put('contacts', contacts);
+  }
+
+  bool _isFormValid() {
+    final isNameNotEmpty = _fnameController.text.trim().isNotEmpty || _lnameController.text.trim().isNotEmpty;
+    final isPhoneNumberNotEmpty = _phoneControllers.any((controller) => controller.text.trim().isNotEmpty);
+    return isNameNotEmpty || isPhoneNumberNotEmpty;
+  }
+
+  void _updateContact(int index, Map<String, dynamic> updatedContact) {
+    setState(() {
+      if (index >= 0 && index < contacts.length) {
+        contacts[index] = {
+          'name': updatedContact['name']?.trim() ?? '',
+          'phone': updatedContact['phone'] ?? '',
+          'email': updatedContact['email'] ?? '',
+          'url': updatedContact['url'] is String
+              ? [updatedContact['url']]
+              : (updatedContact['url'] is List ? updatedContact['url'] : []),
+          'photo': updatedContact['photo'] ?? '',
+        };
+        contacts.sort((a, b) => (a['name'] as String).toLowerCase().compareTo((b['name'] as String).toLowerCase()));
+        _filterContacts();
+        _saveContacts();
+      }
+    });
+  }
+
+  Future<void> _pickImage(BuildContext context, StateSetter setModalState) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setModalState(() {
+        _imagePath = pickedFile.path;
+      });
+
+      // Save image path to local storage using Hive
+      var box = Hive.box('database');
+      box.put('imagePath', pickedFile.path); // Save the path
+
+      print("Image path saved to local storage: ${pickedFile.path}");
+    }
+  }
+
+
+  void _formatPhoneNumber(TextEditingController controller, String value, StateSetter setModalState) {
+    String digitsOnly = value.replaceAll(RegExp(r'[^0-9]'), '');
+    if (digitsOnly.length > 12) {
+      digitsOnly = digitsOnly.substring(0, 12);
+    }
+    controller.value = TextEditingValue(
+      text: digitsOnly,
+      selection: TextSelection.collapsed(offset: digitsOnly.length),
+    );
+    if (digitsOnly.length >= 4) {
+      String formatted = '(${digitsOnly.substring(0, 4)})';
+      if (digitsOnly.length > 4) {
+        formatted += ' ${digitsOnly.substring(4)}';
+      }
+      controller.value = TextEditingValue(
+        text: formatted,
+        selection: TextSelection.collapsed(offset: formatted.length),
+      );
+    }
+    setModalState(() {});
+  }
+
+  void _addPhoneField(StateSetter setModalState) {
+    setModalState(() {
+      _phoneControllers.add(TextEditingController());
+      _phoneLabels.add('phone');
+    });
+  }
+
+  void _removePhoneField(StateSetter setModalState, int index) {
+    if (_phoneControllers.length > 0) {
+      setModalState(() {
+        _phoneControllers.removeAt(index);
+        _phoneLabels.removeAt(index);
+      });
+    }
+  }
+
+  void _addEmailField(StateSetter setModalState) {
+    setModalState(() {
+      _emailControllers.add(TextEditingController());
+      _emailLabels.add('personal');
+    });
+  }
+
+  void _removeEmailField(StateSetter setModalState, int index) {
+    if (_emailControllers.length > 0) {
+      setModalState(() {
+        _emailControllers.removeAt(index);
+        _emailLabels.removeAt(index);
+      });
+    }
+  }
+
+  void _addUrlField(StateSetter setModalState) {
+    setModalState(() {
+      _urlControllers.add(TextEditingController());
+      _urlLabels.add('homepage');
+    });
+  }
+
+  void _removeUrlField(StateSetter setModalState, int index) {
+    if (_urlControllers.length > 0) {
+      setModalState(() {
+        _urlControllers.removeAt(index);
+        _urlLabels.removeAt(index);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        trailing: CupertinoButton(
+          child: Icon(CupertinoIcons.add),
+          onPressed: () {
+            showCupertinoModalPopup(
+              context: context,
+              builder: (context) {
+                return StatefulBuilder(
+                  builder: (context, setModalState) {
+                    return CupertinoActionSheet(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CupertinoButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.pop(context);
+                              _fnameController.clear();
+                              _lnameController.clear();
+                              _phoneControllers = [];
+                              _phoneLabels = [];
+                              _emailControllers = [];
+                              _emailLabels = [];
+                              _urlControllers = [];
+                              _urlLabels = [];
+                              _imagePath = null;
+                            },
+                          ),
+                          Text('New Contact', style: TextStyle(fontSize: 20, color: CupertinoColors.white,) ),
+                          CupertinoButton(
+                            child: Text(
+                              'Done',
+                              style: TextStyle(
+                                color: _isFormValid() ? CupertinoColors.systemBlue : CupertinoColors.inactiveGray,
+                              ),
+                            ),
